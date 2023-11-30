e823d1a8cfea6a702cedd4c2d89abdc8c153532a
MOHD ASEEM AKRAM <113539818+Aseemakram19@users.noreply.github.com>
2023-11-30 21:18:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                body {
+                    background-color: purple;
+                    font-style: italic;
+                    font-family: 'Arial', sans-serif;
+                    color: white;
+                    text-align: center;
+                    padding: 50px;
+                }
+            </style>
+            <title>Welcome to Cloud aseem Youtube channel</title>
+        </head>
+        <body>
+            <h1>Welcome to Cloud aseem Youtube channel</h1>
+            <p>Don't forget to subscribe & share with friends</p>
+        </body>
+        </html>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
