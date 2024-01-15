4137be24622b7c716aca3af71f8bbc614ce58ab5
MOHD ASEEM AKRAM <113539818+Aseemakram19@users.noreply.github.com>
2024-01-15 16:52:47 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+# Navigate to your project directory
+cd /root/project/node-js-docker-cicd
+
+
+# Navigate to your project jenkins/workspace/ directory
+cd /var/lib/jenkins/workspace/nodeapp
+
+# Copy files using rsync
+cp * -r /root/project/node-js-docker-cicd
+
+# Navigate to your project directory
+cd /root/project/node-js-docker-cicd
+
+
+
+# Find the process ID (PID) using port 3000
+PID=$(lsof -t -i:3000)
+
+# If the PID is not empty, kill the process
+if [ -n "$PID" ]; then
+  su -c "kill -9 $PID" -s /bin/bash root
+fi
+
+# Navigate to your project directory
+cd /root/project/node-js-docker-cicd
+
+# stop pm2 process
+pm2 stop ecosystem.config.js
+
+# install node dependency
+npm install express
+
+# Run the deploy script
+nohup node index.js &
+
+# start pm2 process
+pm2 restart ecosystem.config.js
+
+# Find the process ID (PID) using port 3000
+PID=$(lsof -t -i:3000)
+
+# You may want to check if the process is still running after the deploy
+if [ -n "$PID" ]; then
+  echo "The process is still running with PID $PID."
+else
+  echo "Deployment successful.  process running on port 3000."
+fi
