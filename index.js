f81328c01890678ed5286b821bd84f33f6f9543c
techti00 <timoneychibuike@gmail.com>
2025-08-15 16:16:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
             <title>Welcome to Cloud aseem Youtube channel ,Please share & subscribe , Thank you </title>
         </head>
         <body>
-            <h1>Welcome to Cloud aseem Youtube channel ,Please share & subscribe , Thank you very much for 605 subscribers , keep loving , thankyou <3 </h1>
+            <h1>Welcome to Cloud asecribers , keep loving , thankyou <3 </h1>
             <p>Don't forget to coming soon  & share with friends</p>
         </body>
         </html>
